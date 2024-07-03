49c5d0afbbc740368aecbc0927c30ae4830827e7
AhmadHafidz1316 <ahmadhafidzrinoputra@smkwikrama.sch.id>
2024-07-03 18:30:17 +0700

    new-commit

--- index.js
+++ index.js
@@ -11,26 +11,24 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-// Mendapatkan direktori saat ini menggunakan ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Setup multer untuk menyimpan file
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // Rename file dengan timestamp
+    cb(null, Date.now() + path.extname(file.originalname)); 
   }
 });
 
 const upload = multer({ storage });
 
-// Tambahkan multer middleware ke router
-app.use(upload.single('Gambar')); // `Gambar` adalah nama field untuk file
 
-// Menyajikan file statis dari direktori uploads
+app.use(upload.single('Gambar')); 
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(router);
